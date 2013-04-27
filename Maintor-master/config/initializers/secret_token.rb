dbf8169d001dc4de4c0f0f3f73916c29f5b862c1
Kevin <extremesavesgame@gmail.com>
2013-04-26 23:37:59 -0700

    3rd

---
+++ Maintor-master/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = 'e4617292be3e591e3dec9d19a74bc6486e0390d27ff444a1287242f360cd53907b328f97295e4762cc1e4194a61abce6a6abf2d076999f57e0f89474380be638'
